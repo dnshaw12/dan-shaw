460ddd659970ad2930cc7da04f12b2b0aba4318c
Dan <dnshaw12@gmail.com>
2019-08-07 18:05:36 -0500

    adjusted canvas

--- js/app.js
+++ js/app.js
@@ -1,20 +1,35 @@
 const canvas = document.getElementById('my-canvas');
 const ctx = canvas.getContext('2d');
 
+//get canvas
+//get context
+// function fix_dpi() {
+// //get CSS height
+// //the + prefix casts it to an integer
+// //the slice method gets rid of "px"
+// let style_height = +getComputedStyle(canvas).getPropertyValue("height").slice(0, -2);
+// //get CSS width
+// let style_width = +getComputedStyle(canvas).getPropertyValue("width").slice(0, -2);
+// //scale the canvas
+// canvas.setAttribute('height', style_height * dpi);
+// canvas.setAttribute('width', style_width * dpi);
+// }
+
 class Comet {
 
 	constructor(angle, colorIndex){	
-		this.x = -30;
-		this.y = canvas.height/2;
+		this.x = -60;
+		this.y = canvas.height/3;
 		this.angle = angle
 		this.colorIndex = colorIndex
 		this.angleRads = this.angle * (Math.PI/180);
-		this.colors = ['red','orange','yeallow','green','blue','purple','pink']
+		this.colors = ['red','orange','yellow','green','blue','purple','magenta']
 	}
 	draw(){
 		ctx.clearRect(this.x - 4, this.y - 4, 8, 8)
+		// ctx.fillStyle = 'black'
 		ctx.beginPath()
-		ctx.arc(this.x, this.y, 2.5, 0, Math.PI * 2, true)
+		ctx.arc(this.x, this.y, 1.5, 0, Math.PI * 2, true)
 		ctx.fillStyle = this.colors[this.colorIndex];
 		ctx.fill()
 		this.x = this.x + 1 * Math.cos(this.angleRads)
@@ -33,7 +48,7 @@ class Comet {
 
 const cometTimer = setInterval(() => {
 	console.log('newComet');
-	const newComet = new Comet(Math.floor((Math.random() * (35 - -35)) + -35), Math.floor((Math.random() * 8)))
+	const newComet = new Comet(Math.random() * (15 - -15) + -15, Math.floor((Math.random() * 8)))
 	newComet.draw()
 	newComet.animate()
 }, 75)
